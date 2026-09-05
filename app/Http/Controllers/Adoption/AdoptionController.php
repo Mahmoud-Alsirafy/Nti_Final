426e1cef3903ad4da03b014296dfc79d9158a4eb
Mahmoud Alsirafy <alsirafy123@gmail.com>
2026-09-05 11:20:32 +0300

    store_for_adoption api

--- app/Http/Controllers/Adoption/AdoptionController.php
+++ app/Http/Controllers/Adoption/AdoptionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Adoption;
 use App\Http\Controllers\Controller;
 use App\Models\Adoption;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdoptionController extends Controller
 {
@@ -31,5 +32,39 @@ class AdoptionController extends Controller
             ]
         );
         return $adoption;
+    }
+
+    public function store_for_adoption(Request $request)
+    {
+        $validatedData = $request->validate([
+            'pet_id' => 'required|exists:pet_infos,id',
+            'owner_id' => 'required|exists:users,id',
+        ]);
+        DB::beginTransaction();
+        try {
+            $adoption = Adoption::create([
+                "pet_id" => $request->pet_id,
+                "owner_id" => $request->owner_id,
+            ]);
+            DB::commit();
+            // return redirect()->route('Profile.index')->with('success', 'Your personal data has been saved');
+            return "store done succesfully";
+        } catch (\Throwable $e) {
+            DB::rollback();
+            //  return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+            return "false";
+        }
+        return $adoption;
+    }
+
+    public function adoption_status(int $owner_id)
+    {
+        //??remove the $owner_id and use this : return $adoptions = Adoption::where('owner_id', Auth::user()->id())->with('pet', 'owner')->get();
+        $adoptions = Adoption::where('owner_id', $owner_id)->where('status', 'pending')->with('pet', 'owner')->get();
+        if ($adoptions->count() > 0) {
+            return $adoptions;
+        } else {
+            return "no adoption";
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/adoptions', [AdoptionController::class, 'index'])->name('adoptions.index');
+Route::get('/adoption', [AdoptionController::class, 'index'])->name('adoptions.index');
 Route::post('/new_adoption', [AdoptionController::class, 'new_adoption'])->name('new_adoption');
+Route::post('/store_for_adoption', [AdoptionController::class, 'store_for_adoption'])->name('store_for_adoption');
+
+
+// show the pet for adoption by owner
+Route::get('/adoption_status/{owner_id}', [AdoptionController::class, 'adoption_status'])->name('adoption_status');
